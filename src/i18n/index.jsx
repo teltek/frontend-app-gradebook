b52d3e7708829a39096675514e0109ef7c838385
jportela <jose.portela@teltek.es>
2023-10-17 10:39:53 +0200

    Add spanish language

--- src/i18n/index.jsx
+++ src/i18n/index.jsx
@@ -1,12 +1,14 @@
 import arMessages from './messages/ar.json';
 // no need to import en messages-- they are in the defaultMessage field
 import es419Messages from './messages/es_419.json';
+import esesMessages from './messages/es_es.json';
 import frMessages from './messages/fr.json';
 import zhcnMessages from './messages/zh_CN.json';
 
 const messages = {
   ar: arMessages,
   'es-419': es419Messages,
+  'es-es': esesMessages,
   fr: frMessages,
   'zh-cn': zhcnMessages,
 };
